17ff0bf464c880dd973ae7f57f948890d439d548
nigussiefolikia@gmail.com <your@email.com>
2025-06-30 13:15:14 +0300

    Fix task_2.sql with quantity DOUBLE in Order_Details

--- task_2.sql
+++ task_2.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     unit_price DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
