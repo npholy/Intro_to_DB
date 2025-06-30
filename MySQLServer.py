86d37cf3ea61863de16abb3e754d03caced5f4eb
nigussiefolikia@gmail.com <your@email.com>
2025-06-30 12:24:20 +0300

    Add MySQLServer.py with correct exception handling and task_2.sql for table creation

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,12 +2,12 @@ import mysql.connector
 
 def create_database():
     try:
-       connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Pholiki@27",
-    port=3307
-)
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Pholiki@27",
+            port=3307
+        )
         if connection.is_connected():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
